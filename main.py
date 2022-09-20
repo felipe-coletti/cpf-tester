ff9a6124b160364144474443a7296b9724a21ba8
felipe-coletti <83978518+felipe-coletti@users.noreply.github.com>
2022-09-20 11:13:30 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,47 @@
-# solicite o CPF do usuário
-cpf = int(input("CPF: "))
+cpf = '12345678944'
 
 
-# Crie a funcão que verifica o CPF
+# Criar a funcão que verifica o CPF
 def test(result_1=0, result_2=0):
     # primeiro número * 10, segundo número * 9... Até o último número antes do traço
-    for i in range(len(cpf)):
-        number = len(cpf) - 1 - i
-        result_1 += cpf[i] * number
+    for i in range(len(cpf) - 2):
+        number = len(cpf) + 1 - i
+        result_1 += int(cpf[i]) * number
     # primeiro número * 11, segundo número * 10... Até o penúltimo número
-    for i in range(len(cpf)):
-        number = len(cpf) - i
-        result_2 += cpf[i] * number
-    # ache o número de verificação
-    rest_1 = result_1 * 10 // 11
-    rest_2 = result_2 * 10 // 11
-    # transforme o resto das divisões num único digito, caso ainda não seja
+    for i in range(len(cpf) - 1):
+        number = len(cpf) + 1 - i
+        result_2 += int(cpf[i]) * number
+    # achar o número de verificação
+    print(result_1)
+    print(result_2)
+    rest_1 = result_1 * 10 % 11
+    rest_2 = result_2 * 10 % 11
+    print(rest_1)
+    print(rest_2)
+    # transformar num único digito, caso ainda não seja
     if rest_1 == 10:
         rest_1 = 0
     if rest_2 == 10:
         rest_2 = 0
-    # fazça a validação do CPF
-    if rest_1 != cpf[10] and rest_2 != cpf[11]:
+    print(rest_1)
+    print(rest_2)
+    # fazer a validação do CPF
+    print(rest_1)
+    print(cpf[10])
+    print(rest_2)
+    print(cpf[10])
+    if rest_1 == cpf[10] and rest_2 == cpf[11]:
         validation = True
     else:
         validation = False
     return validation
 
 
-# retire todos os caracteres e/ou letras do número de CPF
+# retirar todos os caracteres e/ou letras do número de CPF
 cpf = ''.join(i for i in cpf if i.isdigit())
 
-# exiba a saída
-if test:
+# exibir o resultado
+if test():
     print('CPF válido.')
 else:
     print('CPF inválido.')
