67554f505675a823e21181a1de94a7eab0851742
felipe-coletti <83978518+felipe-coletti@users.noreply.github.com>
2022-09-20 10:30:22 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-# CPF de exemplo
-cpf = '123.456.789-10'
+# solicite o CPF do usuário
+cpf = int(input("CPF: "))
 
 
-# Criar a funcão que verifica o CPF
+# Crie a funcão que verifica o CPF
 def check(result_1=0, result_2=0):
     # primeiro número * 10, segundo número * 9... Até o último número antes do traço
     for i in range(len(cpf)):
@@ -12,15 +12,15 @@ def check(result_1=0, result_2=0):
     for i in range(len(cpf)):
         number = len(cpf) - i
         result_2 += int(cpf[i]) * number
-    # achar o número de verificação
+    # ache o número de verificação
     result_1 = result_1 * 10 // 11
     result_2 = result_2 * 10 // 11
-    # transformar num único digito, caso ainda não seja
+    # transforme o resto das divisões num único digito, caso ainda não seja
     if result_1 == 10:
         result_1 = 0
     if result_2 == 10:
         result_2 = 0
-    # fazer a validação do CPF
+    # fazça a validação do CPF
     if result_1 != cpf[10] and result_2 != cpf[11]:
         validation = True
     else:
@@ -28,10 +28,10 @@ def check(result_1=0, result_2=0):
     return validation
 
 
-# retirar todos os caracteres e/ou letras do número de CPF
+# retire todos os caracteres e/ou letras do número de CPF
 cpf = ''.join(i for i in cpf if i.isdigit())
 
-# exibir o resultado
+# exiba a saída
 if check:
     print('CPF válido.')
 else:
