538fb0535e580f32320dfa13694a6769273221af
felipe-coletti <83978518+felipe-coletti@users.noreply.github.com>
2022-10-06 10:36:58 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,20 @@
-cpf = '12345678909'
+cpf = input('CPF: ')
 
 
 def test(result_1=0, result_2=0):
-    print(len(cpf))
     for i in range(len(cpf) - 2):
         number = len(cpf) - 1 - i
         result_1 += int(cpf[i]) * number
-        print(i)
     for i in range(len(cpf) - 1):
         number = len(cpf) - i
         result_2 += int(cpf[i]) * number
-        print(i)
-    print(result_1)
-    print(result_2)
     rest_1 = (result_1 * 10) % 11
     rest_2 = (result_2 * 10) % 11
-    print(rest_1)
-    print(rest_2)
     if rest_1 == 10:
         rest_1 = 0
     if rest_2 == 10:
         rest_2 = 0
-    print(rest_1)
-    print(rest_2)
-    print(cpf[9])
-    print(cpf[10])
-    if rest_1 == cpf[len(cpf) - 2] and rest_2 == cpf[len(cpf) - 1]:
+    if rest_1 == int(cpf[len(cpf) - 2]) and rest_2 == int(cpf[len(cpf) - 1]):
         return True
     else:
         return False
